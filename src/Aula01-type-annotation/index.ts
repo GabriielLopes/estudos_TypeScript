fe9602c54e2af0c65f7c807cae93573579133c94
Gabriel Lopes <136989000+GabriielLopes@users.noreply.github.com>
2024-09-07 18:02:31 -0300

    aula type annotation

---
+++ src/Aula01-type-annotation/index.ts
@@ -0,0 +1,32 @@
+/* eslint-disable */
+
+// tipos básicos (aqui ocorre a inferência de tipos)
+let nome: string = 'Gabriel';
+let idade: number = 23;
+let adulto: boolean = true;
+let simbolo: symbol = Symbol('Qualquer-symbol');
+// let big: bigint = 10n;
+
+// arrays
+
+let arraysDeNumeros: Array<number> = [1,2,5];
+let arrayDeNumeros2: number[] = [1,2,5];
+let arraysDeStrings: Array<string> = ['Gabriel', 'Karla', 'Elias'];
+let arraydeStrings2: string[] = ['a','b','c'];
+
+// Objetos
+
+let pessoa: {nome: string, idade: number, adulto?: boolean} = {
+  nome: 'Gabriel',
+  idade: 15
+}
+
+// Funções
+
+function soma(x: number, y: number) {
+  return x + y;
+}
+
+const result = soma(2,3);
+
+const soma2: (x: number, y: number) => number = (x, y) => x + y;
