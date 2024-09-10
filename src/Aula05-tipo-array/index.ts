86084f0bdfa19f440d420db3507d146add4d90c9
Gabriel Lopes <Gabrielcostaplay@gmail.com>
2024-09-10 12:42:19 -0300

    Tipo array

---
+++ src/Aula05-tipo-array/index.ts
@@ -0,0 +1,19 @@
+// Array<T> ou T[]
+
+export function multiplicaArgs(...args: Array<number>): number {
+  return args.reduce((acumulador, valor) => acumulador * valor,1)
+}
+
+console.log(multiplicaArgs(1,5,3))
+
+export function concatenaString(...args: string[]) {
+  return args.reduce((acumulador, valor) => acumulador + valor);
+}
+
+console.log(concatenaString('gabriel', 'lopes'))
+
+export function toUpperCase(...args: string[]): string[] {
+  return args.map((valor) => valor.toUpperCase());
+}
+
+console.log(toUpperCase('Gabriel', 'lopes'));
