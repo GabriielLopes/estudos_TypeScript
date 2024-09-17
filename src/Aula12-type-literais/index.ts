3c1f0c85b36b36d3a8a9c26e3877eb7bb08f7f23
Gabriel Lopes <Gabrielcostaplay@gmail.com>
2024-09-17 12:06:36 -0300

    Add Aula12 tipos literais

---
+++ src/Aula12-type-literais/index.ts
@@ -0,0 +1,11 @@
+const y = 10;
+//let a: 100 = 100; // devemos utilizar const
+
+function escolhaCor(cor: 'Vermelho' | 'Amarelo' | 'Preto'): string {
+  return cor;
+}
+
+console.log(escolhaCor("Amarelo"));
+
+// Module Mode
+export default 1;
