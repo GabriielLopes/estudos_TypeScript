405f51f45bb9f579e432701852175a255b2481a1
Gabriel Lopes <Gabrielcostaplay@gmail.com>
2024-09-19 12:12:17 -0300

    Add Aula17 type assertions

---
+++ src/Aula17-type-assertions/index.ts
@@ -0,0 +1,15 @@
+// --------------------- Condicional ---------------------
+const body = document.querySelector('body');
+if (body) body.style.background = 'red';
+
+// --------------------- Non-null assertion (!) ---------------------
+const body2 = document.querySelector('body')!;
+body2.style.background = 'red';
+
+// --------------------- Type assertion ---------------------
+const body3 = document.querySelector('body') as HTMLBodyElement;
+body3.style.background = 'red';
+
+// --------------------- HTMLElement ---------------------
+const input = document.querySelector('.input') as HTMLInputElement;
+input.value = 'VALOR';
