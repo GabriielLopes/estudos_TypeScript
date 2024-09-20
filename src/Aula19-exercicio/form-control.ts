294374d63490a2a02a4c7eadf1fb3062ff5cb8a5
Gabriel Lopes <Gabrielcostaplay@gmail.com>
2024-09-20 16:38:19 -0300

    modified:   src/Aula19-exercicio/form-control.ts

--- src/Aula19-exercicio/form-control.ts
+++ src/Aula19-exercicio/form-control.ts
@@ -6,9 +6,7 @@ const form = document.querySelector('.form') as HTMLFormElement;
 const username = document.querySelector('.username') as HTMLInputElement;
 const email = document.querySelector('.email') as HTMLInputElement;
 const password = document.querySelector('.password') as HTMLInputElement;
-const confirmPassword = document.querySelector(
-  '.confirmpassword',
-) as HTMLInputElement;
+const confirmPassword = document.querySelector('.confirmpassword') as HTMLInputElement;
 
 form.addEventListener('submit', function (event: Event) {
   event.preventDefault();
@@ -21,36 +19,27 @@ form.addEventListener('submit', function (event: Event) {
 });
 
 function hideErrorMessages(form: HTMLFormElement): void {
-  form
-    .querySelectorAll('.' + SHOW_MESSAGE_ERROR)
-    .forEach((item) => item.classList.remove(SHOW_MESSAGE_ERROR));
+  form.querySelectorAll('.' + SHOW_MESSAGE_ERROR).forEach((item) => item.classList.remove(SHOW_MESSAGE_ERROR));
 }
 
 function showErrorMessage(input: HTMLInputElement, msg: string): void {
   const form_fields = input.parentElement as HTMLDivElement; // irá capturar o pai do elemento, noo caso 'form-fields'
-  const errorMessage = form_fields.querySelector(
-    '.info-erro',
-  ) as HTMLParagraphElement;
+  const errorMessage = form_fields.querySelector('.info-erro') as HTMLParagraphElement;
   errorMessage.innerHTML = msg;
   form_fields.classList.add(SHOW_MESSAGE_ERROR);
 }
 
 function checkForEmptyFields(...inputs: HTMLInputElement[]): void {
   inputs.forEach((input) => {
-    if (!input.value)
-      showErrorMessage(input, '* Este campo não pode ficar vazio!');
+    if (!input.value) showErrorMessage(input, '* Este campo não pode ficar vazio!');
   });
 }
 
 function checkEmail(input: HTMLInputElement): void {
-  if (!isEmail(input.value))
-    showErrorMessage(input, '* O e-mail informado é inválido!');
+  if (!isEmail(input.value)) showErrorMessage(input, '* O e-mail informado é inválido!');
 }
 
-function checkEqualPasswords(
-  password: HTMLInputElement,
-  confirmPassword: HTMLInputElement,
-): void {
+function checkEqualPasswords(password: HTMLInputElement, confirmPassword: HTMLInputElement): void {
   if (password.value !== confirmPassword.value) {
     showErrorMessage(password, '');
     showErrorMessage(confirmPassword, 'As senhas não conferem!');
