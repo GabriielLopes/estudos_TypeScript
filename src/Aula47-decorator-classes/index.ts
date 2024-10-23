8c147b1222e704c0a14cf955446091405ea3512f
Gabriel Lopes <Gabrielcostaplay@gmail.com>
2024-10-23 10:40:28 -0300

    Aula47

--- src/Aula47-decorator-classes/index.ts
+++ src/Aula47-decorator-classes/index.ts
@@ -1,4 +1,4 @@
-@decorator
+@decorator('Valor1', 'Valor2')
 export class Animal {
   constructor(public cor: string) {}
 }
@@ -8,15 +8,19 @@ export class Animal {
   return target;
 } */
 
-function decorator<T extends new (...args: any[]) => any>(target: T): T {
-  return class extends target {
-    cor: string;
+function decorator(param1: string, param2: string) {
+  // Closure
+  console.log(param1, param2);
+  return function <T extends new (...args: any[]) => any>(target: T): T {
+    return class extends target {
+      cor: string;
 
-    constructor(...args: any[]) {
-      super(...args);
-      this.cor = args[0].split('').reverse().join('');
-    }
-  }
+      constructor(...args: any[]) {
+        super(...args);
+        this.cor = args[0].split('').reverse().join('');
+      }
+    };
+  };
 }
 
 const animal = new Animal('Amarelo');
