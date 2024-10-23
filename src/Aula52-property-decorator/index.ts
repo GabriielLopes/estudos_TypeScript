5116cc2124d6d62d5a5cf7f67beabe8c144ddc36
Gabriel Lopes <Gabrielcostaplay@gmail.com>
2024-10-23 12:38:12 -0300

    Aula52 - decoradores de propriedades

---
+++ src/Aula52-property-decorator/index.ts
@@ -0,0 +1,44 @@
+function decorador(classProtoType: any, nome: string | symbol): any {
+  let valorPropriedade: any;
+  return {
+    get: () => valorPropriedade,
+    set: (valor: any) => {
+      if (typeof valor  != 'string') return
+      return valorPropriedade = valor.split('').reverse().join('');
+    },
+  }
+}
+
+export class UmaPessoa {
+  @decorador
+  nome: string;
+  @decorador
+  sobrenome: string;
+  @decorador
+  idade: number;
+
+  constructor(nome: string, sobrenome: string, idade: number) {
+    this.nome = nome;
+    this.sobrenome = sobrenome;
+    this.idade = idade;
+  }
+
+  metodo(msg: string): string {
+    return `${this.nome} ${this.sobrenome} ${msg}`;
+  }
+
+  get nomeCompleto(): string {
+    return this.nome + ' ' + this.sobrenome;
+  }
+
+  set nomeCompleto(valor: string) {
+    const palavras = valor.split(/\s+/g);
+    const primeiroNome = palavras.shift();
+    if (!primeiroNome) return;
+    this.nome = primeiroNome;
+    this.sobrenome = palavras.join(' ');
+  }
+}
+const pessoa = new UmaPessoa('GABRIEL', 'LOPES', 25);
+const metodo = pessoa.metodo('Olá mundo!');
+console.log(metodo);
